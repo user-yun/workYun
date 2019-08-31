320bfd34d23357785b8cfb2113ba3560ce9af2a0
user-yun <xuyunfei.mail@qq.com>
2019-08-31 18:58:00 +0800

    方法添加

--- src/f.js
+++ src/f.js
@@ -4,3 +4,16 @@ Array.prototype.max = function () {
 Array.prototype.min = function () {
     return Math.min.apply({}, this);
 }
+Array.prototype.sums = function (i) {
+    let s = 0;
+    if (i == undefined) {
+        this.forEach(e => {
+            s += parseInt(e);
+        });
+    } else {
+        this.forEach(e => {
+            s += parseInt(e[i]);
+        });
+    }
+    return s;
+}
